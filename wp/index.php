e9d9773f4fbaf9ec185a4e0ee92f195fabca4d77
hiby90hou <hiby.90hou@qq.com>
2017-07-05 12:53:17 +1000

    add worpress

---
+++ wp/index.php
@@ -0,0 +1,328 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Crossculture Christian Club</title>
+    <link href="<?php bloginfo('stylesheet_url');?>" rel="stylesheet">
+    <!-- link openSan font -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+    <!-- nav -->
+    <div class="container-fluid">
+        <nav class="navbar my-navbar navbar-fixed-top">
+            <div class="container-fluid">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#"><img src="<?php echo get_template_directory_uri();?>/images/focus.png" alt=""></a>
+                </div>
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="#">Home</a></li>
+                        <li class="my-dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">What We Do<span class="caret"></span></a>
+                            <ul class="my-dropdown-menu">
+                                <li><a href="#">Thursday Meeting</a></li>
+                                <li><a href="#">Our Events</a></li>
+                            </ul>
+                        </li>
+                        <li class="my-dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">About Us<span class="caret"></span></a>
+                            <ul class="my-dropdown-menu">
+                                <li><a href="#">About Us</a></li>
+                                <li><a href="#">Our History</a></li>
+                                <li><a href="#">Our Team</a></li>
+                                <li><a href="#">Bible Study Materials</a></li>
+                                <li role="separator" class="divider"></li>
+                                <li><a href="#">Contact Us</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#">Get Involved</a></li>
+                    </ul>
+                </div>
+                <!-- /.navbar-collapse -->
+            </div>
+            <!-- /.container-fluid -->
+        </nav>
+    </div>
+    <!-- banner -->
+    <div class="banner-box">
+        <a class="banner1" href="#">
+            <div>MAKE FRIENDS,
+                <br> MEET JESUS.</div>
+        </a>
+    </div>
+    <!-- button box -->
+    <div class="container-fluid white_bg">
+        <div class="button_box">
+            <div class="row">
+                <div class="col-md-4">
+                    <button type="button" class="btn btn-info">What's on</button>
+                </div>
+                <div class="col-md-4">
+                    <button type="button" class="btn btn-info">FAQ</button>
+                </div>
+                <div class="col-md-4">
+                    <button type="button" class="btn btn-info">I am new</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- introduction -->
+    <div class="jumbotron introduction white_bg">
+        <div class="container-fluid">
+            <p>Hi, international student!</br>
+                Do you feel lonely because you do not have any friend in Australia?</br>
+                Come to Crossculture! Let's make friends and meet Jesus!
+            </p>
+        </div>
+    </div>
+    <!-- about crossculture -->
+    <div class="jumbotron about_Crossculture">
+        <div class="container-fluid">
+            <h1>Crossculture Christian Club</h1>
+            <h2>Make friends and meet Jesus</h2>
+            <p>We are a mix of Australians and Internationals who are passionate about the relationship we can have with God because Jesus died for us.</br>
+                </br>
+                Our main meeting is every Thursday night at 6:00 pm. We also have many other events and activities such as free English class, camps and girls night.
+            </p>
+        </div>
+    </div>
+    <!-- main meeting -->
+    <div class="container-fluid introduction_pic_box">
+        <img class="img-responsive" src="<?php echo get_template_directory_uri();?>/images/meeting.jpg">
+    </div>
+    <div class="jumbotron introduction">
+        <div class="container-fluid">
+            <h2>Thursday Night Meeting</h2>
+            <p>We meet Thursday night every week in ST. JUDE'S church, Carlton. In our meeting, we make friends, have dinner, study bible and have social talk with dessert!</br>
+                </br>
+                If you are interesting, please record the following info in your calendar:</br>
+                </br>
+                </br>
+            </p>
+            <!--         <p>
+        •Time:     every Thursday 6pm</br>
+        •Cost:      dinner is $5 per week </br>
+                            (if it is your first time,dinner is free)</br>
+        •Place:     ST. JUDE'S ANGLICAN CHURCH</br>
+                            2 KEPPEL ST, CARLTON, VIC, 3053</br>
+        •Agenda:</br>
+        </p>  -->
+            <div class="container-fluid info_table">
+                <div class="row">
+                    <div class="col-md-4 col-sm-6">
+                        •Time:
+                    </div>
+                    <div class="col-md-8 col-sm-6">
+                        every Thursday 6pm</br>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-4 col-sm-6">
+                        •Cost:
+                    </div>
+                    <div class="col-md-8 col-sm-6">
+                        dinner is $5 per week </br>
+                        <span>(if it is your first time,dinner is free)</span>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-4 col-sm-6">
+                        •Place:
+                    </div>
+                    <div class="col-md-8 col-sm-6">
+                        ST. JUDE'S Anglican Church</br>
+                        <span>2 KEPPEL ST, CARLTON, VIC, 3053</span>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-4 col-sm-6">
+                        •Agenda:
+                    </div>
+                    <div class="col-md-8 col-sm-6">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- clock -->
+        <div class="container-fluid time_table">
+            <div class="row">
+                <div class="col-md-4">
+                    <p>6:00</p>
+                    <div class="circlePic img-circle pic1"></div>
+                    <p>Make friends & </br> have Dinner</p>
+                </div>
+                <div class="col-md-4">
+                    <p>7:00</p>
+                    <div class="circlePic img-circle pic2"></div>
+                    <p>Pray for Country & </br> Bible Study</p>
+                </div>
+                <div class="col-md-4">
+                    <p>8:30</p>
+                    <div class="circlePic img-circle pic3"></div>
+                    <p>Social Talk & </br> Dessert Time</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+    <!-- free English class -->
+    <div class="jumbotron English_class">
+        <div class="container-fluid">
+            <h2>Free English class</h2>
+            <p>Cannot speak English well? Want to improve your English?</br>
+                At St Jude's church in Carlton (235 Palmerston Street), we are holding FREE English classes every Thursday 5-6 pm, every week of the year.
+            </p>
+        </div>
+    </div>
+    <!-- about our event -->
+    <div class="jumbotron about_event">
+        <div class="container-fluid">
+            <h2>We have Events</h2>
+            <!-- <div class="event_pic"></div> -->
+            <!-- carousel for mobile devices-->
+            <div class="carouselSm hidden-md hidden-lg">
+                <ul class="carousel_imgs clearfix">
+                    <li>
+                        <a href="#"><img src="<?php echo get_template_directory_uri();?>/images/carousel_5.jpg" alt=""></a>
+                    </li>
+                    <li>
+                        <a href="#"><img src="<?php echo get_template_directory_uri();?>/images/carousel_1.jpg" alt=""></a>
+                    </li>
+                    <li>
+                        <a href="#"><img src="<?php echo get_template_directory_uri();?>/images/carousel_2.jpg" alt=""></a>
+                    </li>
+                    <li>
+                        <a href="#"><img src="<?php echo get_template_directory_uri();?>/images/carousel_3.jpg" alt=""></a>
+                    </li>
+                    <li>
+                        <a href="#"><img src="<?php echo get_template_directory_uri();?>/images/carousel_4.jpg" alt=""></a>
+                    </li>
+                    <li>
+                        <a href="#"><img src="<?php echo get_template_directory_uri();?>/images/carousel_5.jpg" alt=""></a>
+                    </li>
+                    <li>
+                        <a href="#"><img src="<?php echo get_template_directory_uri();?>/images/carousel_1.jpg" alt=""></a>
+                    </li>
+                </ul>
+                <ul class="carousel_index clearfix">
+                    <li class='current'></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
+            <!-- carousel for pc -->
+            <div id="carousel-example-generic" class="carousel slide hidden-xs hidden-sm" data-ride="carousel">
+                <!-- Indicators -->
+                <ol class="carousel-indicators">
+                    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                    <li data-target="#carousel-example-generic" data-slide-to="3"></li>
+                    <li data-target="#carousel-example-generic" data-slide-to="4"></li>
+                </ol>
+                <!-- Wrapper for slides -->
+                <div class="carousel-inner" role="listbox">
+                    <div class="item active">
+                        <div class='pc_carousel_img image_1'></div>
+                    </div>
+                    <div class="item">
+                        <div class='pc_carousel_img image_2'></div>
+                    </div>
+                    <div class="item">
+                        <div class='pc_carousel_img image_3'></div>
+                    </div>
+                    <div class="item">
+                        <div class='pc_carousel_img image_4'></div>
+                    </div>
+                    <div class="item">
+                        <div class='pc_carousel_img image_5'></div>
+                    </div>
+                </div>
+                <!-- Controls -->
+                <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+                    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+                    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+        </div>
+    </div>
+    <!-- who can join us -->
+    <div class="container-fluid  join_us hidden-xs  white_bg">
+        <div class="origin_point">
+            <div class="circle1 img-circle">
+                <p>
+                    <span>Who</span></br>
+                    Can Join Us
+                </p>
+            </div>
+            <div class="circle2 img-circle"></div>
+            <div class="circle3 img-circle">
+                <p>International student welcome</p>
+            </div>
+            <div class="circle4 img-circle">
+                <p>Every school / university's student welcome</p>
+            </div>
+            <div class="circle5 img-circle">
+                <p>All English Level welcome</p>
+            </div>
+            <div class="circle6 img-circle">
+                <p>Christian / non-Christian welcome</p>
+            </div>
+        </div>
+    </div>
+    <!-- like us on facebook -->
+    <!--     <div class="container-fluid facebook_box">
+    </div> -->
+    <!-- footer -->
+    <div class="container-fluid footer">
+        <p>ST. JUDE'S ANGLICAN CHURCH CARLTON, 2 KEPPEL ST, CARLTON, VIC, 3053 AUSTRALIA </br> AUSSIEPRAYER@GMAIL.COM</p>
+        </br>
+        <p>FOLLOW US on FACEBOOK</p>
+        </br>
+        <div class="footer_fb">
+            <iframe src="https://www.facebook.com/plugins/follow.php?href=https%3A%2F%2Fwww.facebook.com%2Fcrosscultures.melbourne%2F&width=80&height=65&layout=button&size=large&show_faces=false&appId" width="80" height="65" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
+        </div>
+        <div class='footer_link'>
+            <div><a href="">ABOUT US</a></div>
+            <div><a href="">CONTACT US</a></div>
+            <div><a href="">PARTNER SITES</a></div>
+            <div><a href="">USEFUL LINKS</a></div>   
+        </div>
+        <div class='footer_policy'><a href="">Privacy Policy</a></div>
+    </div>
+</body>
+
+</html>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="<?php echo get_template_directory_uri();?>/lib/js/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="<?php echo get_template_directory_uri();?>/lib/js/bootstrap.min.js"></script>
+<!-- carousel -->
+<script src="<?php echo get_template_directory_uri();?>/js/carousel.js"></script>
